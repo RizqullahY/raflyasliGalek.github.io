65a522348c025ce984074414265ad7d06b513520
Rafly Rizqullah <rizqullahyusufrafly@gmail.com>
2024-09-14 12:48:47 +0700

    mencoba iklan

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,42 @@
+//! Navbar Fixed
+// window.onscroll = function(){
+//    const header = document.querySelector('#header');
+//    const fixedNav = header.offsetTop;
+
+//    if(window.pageYOffset > fixedNav){
+//       header.classList.add('navbar-fixed');
+//    }else{
+//       header.classList.remove('navbar-fixed');
+//    }
+// }
+
 // Hamburger
-const hamburger = document.querySelector("#hamburger"), navMenu = document.querySelector("#nav-menu");
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", function () {
-	hamburger.classList.toggle("hamburger-active"),
-		navMenu.classList.toggle("hidden");
-}),
+	hamburger.classList.toggle("hamburger-active");
+	navMenu.classList.toggle("hidden");
+});
 
 // Klik diluar hamburger
 window.addEventListener("click", function (e) {
-	e.target != navMenu &&
-		e.target != hamburger &&
-		(hamburger.classList.remove("hamburger-active"),
-		navMenu.classList.add("hidden"));
+	if (e.target != navMenu && e.target != hamburger) {
+		hamburger.classList.remove("hamburger-active");
+		navMenu.classList.add("hidden");
+	}
 });
 
 // Dark Mode
-const darkToggle = document.querySelector("#dark-toggle"), html = document.querySelector("html");
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
 darkToggle.addEventListener("click", function () {
-	darkToggle.checked
-		? html.classList.add("dark")
-		: html.classList.remove("dark");
+	if (darkToggle.checked) {
+		html.classList.add("dark");
+	} else {
+		html.classList.remove("dark");
+	}
 });
+
+
+
